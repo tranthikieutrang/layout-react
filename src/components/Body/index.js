b878fb41843a736be2c4d0c60fcb5669731ddddd
TranThiKieuTrang <tranthikieuden@gmail.com>
2022-09-03 18:13:08 +0700

    add components folder bai tap buoi 1 react

---
+++ src/components/Body/index.js
@@ -0,0 +1,12 @@
+import Banner from "./Banner";
+import Item from "./Item";
+const Body = () => {
+  return (
+    <div>
+      <Banner />
+      <Item />
+    </div>
+  );
+};
+
+export default Body;
